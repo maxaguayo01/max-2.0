4a7d9e0bc602e8ecc4bcc98fda1d784eb81c47a3
maxaguayo01 <32375373+maxaguayo01@users.noreply.github.com>
2017-09-28 17:22:34 -0500

    Initial commit

---
+++ Assets/enemyia.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyia : MonoBehaviour {
+
+	protected NatMeshAgent agent;
+
+	public float walkspeed = 2f;
+	public float turnspeed = 180f;
+
+	// Use this for initialization
+	void Start () {
+	
+		agent = GetComponent <NavMeshAgent> ();
+
+		if(agent)
+		{
+			Debug.LogError(name + "has no NavMeshAgent")
+			enabled = false;
+			return;
+
+	
+	    
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
